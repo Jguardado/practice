fc4e16c0e2e62060560ae68c515ad6e072ddb474
Juan <jguardado85@gmail.com>
2016-01-09 19:00:02 -0800

    making progress

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
-var db = require('./serverSide/database/app.js');
+var db = require('./serverSide/database/db.js');
 //var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('./client'));
+
+app.get('/data', function(req, res, next){
+  db.users.find({}, function(err, data){
+    if(err){
+      res.status(500);
+    }else{
+      res.status(200).send(data);
+    }
+  })
+});
 
 // app.get('/', function(req, res, next){
 //   console.log('insde something');
